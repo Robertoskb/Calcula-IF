49e3b3e19f5c3d9d22880adb59f06609744ed9ec
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-01-31 10:13:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Valores importates para o calculo da media do aluno
 let peso1 = 2
 let peso2 = 2
 let peso3 = 3
@@ -16,6 +17,9 @@ function calculate(){
 
     if (score5 < 0 || score5 > 100){createtags.innerHTML='<h1>Notas Invalidas</h1>'; return}
 
+    /*verifca se algumas das quatro primeiras notas são invalidas, a quinta nota a quinta nota é a da prova final e pode ou não ser NaN
+    */
+
     let arr = [score1, score2, score3, score4]
     for (let pos in arr){
         if(arr[pos] > 100 || arr[pos] < 0 || isNaN(arr[pos])){
@@ -24,14 +28,16 @@ function calculate(){
         }
     }
 
+    /* Se score5 não for um numero signfica que o aluno  não fez a prova final */
+
     createtags.innerHTML = (isNaN(score5)) 
     ? no_final_test(score1, score2, score3, score4) 
     : final_test(score1, score2, score3, score4, score5)    
 }
 
 function no_final_test(score1, score2, score3, score4){ 
-    let media =  Math.round((peso1*score1 + peso2*score2 + peso3*score3 + peso4*score4)/10)
-    let resultado = result(media, false, score1, score2, score3, score4)
+    let media =  Math.round((peso1*score1 + peso2*score2 + peso3*score3 + peso4*score4)/10) //calcula a media dos quatro bimestres sem a prova final
+    let resultado = result(media, false, score1, score2, score3, score4) //chama a função result que retorna a situação do aluno e uma mensagem
     let sit = resultado.sit
     let msg = resultado.msg
 
@@ -69,7 +75,7 @@ function result(media, provafinal = false, score1, score2, score3, score4){
 }
 
 function final_test(score1, score2, score3, score4, score5){
-    let media = media_final(score1, score2, score3, score4, score5)
+    let media = media_final(score1, score2, score3, score4, score5) 
     let resultado = result(media, true, score1, score2, score3, score4)
     let msg = resultado.msg
     let sit = resultado.sit
@@ -84,7 +90,8 @@ function final_test(score1, score2, score3, score4, score5){
     <p>${msg}</p>`
 }
 
-function media_final(score1, score2, score3, score4, score5){
+function media_final(score1, score2, score3, score4, score5){ 
+    //calcula a nota do aluno com a prova final, são 5 formulas, a que tiver o maior resultado será retornada
     let simple_score = Math.round(((((score1 * peso1) + (score2 * peso2) + (score3 * peso3) + (score4 * peso4)) / soma_pesos) + score5) / 2)
     let scoreSub1 = Math.round(((score5 * peso1) + (score2 * peso2) + (score3 * peso3) + (score4 * peso4))/soma_pesos)
     let scoreSub2 = Math.round(((score1 * peso1) + (score5 * peso2) + (score3 * peso3) + (score4 * peso4))/soma_pesos)
@@ -95,6 +102,7 @@ function media_final(score1, score2, score3, score4, score5){
 }
 
 function score_min(media, score1, score2, score3, score4){
+    //calcula a nota necessaria na prova final, são 5 formulas, a que tiver o menor resultado retornada
     let simple_score = (media_necessaria * 2) - media
     let scoreSub1 =  (score_necessario - ((score2*peso2) + (score3*peso3) + (score4*peso4)))/peso1
     let scoreSub2 =  (score_necessario - ((score1*peso1) + (score3*peso3) + (score4*peso4)))/peso2

8a494bc25268cffffebe00546f32428d324d5806
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-02-03 10:08:58 -0300

    .

--- style.js
+++ style.js
@@ -1,11 +1,12 @@
 let el = document.getElementById('b')
+el.addEventListener('touchstart', () => el.classList.add('touchenter'))
+el.addEventListener('touchend', () => el.classList.remove('touchenter'))
+
 el.addEventListener('mouseenter', () => el.classList.add('enter'))
 el.addEventListener('mouseout', () => el.classList.remove('enter'))
 
 el.addEventListener('mousedown', () => el.classList.add('mousedown'))
 el.addEventListener('mouseup', () => el.classList.remove('mousedown'))
 
-el.addEventListener('touchstart', () => el.classList.add('touchenter'))
-el.addEventListener('touchend', () => el.classList.remove('touchenter'))
 
 

138551c071a5e77be8b6c4881b5f5748baeb3218
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-02-03 09:24:24 -0300

    touch

--- style.js
+++ style.js
@@ -2,4 +2,7 @@ let el = document.getElementById('b')
 el.addEventListener('mousedown', () => el.classList.add('mousedown'))
 el.addEventListener('mouseup', () => el.classList.remove('mousedown'))
 
+el.addEventListener('touchstart', () => el.classList.add('mousedown'))
+el.addEventListener('touchend', () => el.classList.remove('mousedown'))
+
 

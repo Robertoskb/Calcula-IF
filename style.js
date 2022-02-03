2846695c688a50b9c24036c264ea421054447ba6
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-02-03 12:01:41 -0300

    .

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
 let el = document.getElementById('b')
+let checkDevice = Modernizr.touch 
 
-if (!checkDevice()){
+if (!checkDevice){
 el.addEventListener('mouseenter', () => el.classList.add('enter'))
 el.addEventListener('mouseout', () => el.classList.remove('enter'))
 
@@ -13,21 +14,4 @@ else{
     el.addEventListener('touchend', () => el.classList.remove('touchenter'))
 }
 
-
-function checkDevice() { 
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ){
-       return true;
-     }
-    else {
-       return false; 
-     }
-   }
-   
 
